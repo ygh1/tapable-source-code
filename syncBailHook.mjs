95e00eba70e07e90c56a5264a8c4478335d75249
yanguanghao <yanguanghao@qihangxingchen.com>
2021-09-07 10:19:22 +0800

    熔断钩子

--- syncBailHook.mjs
+++ syncBailHook.mjs
@@ -1,9 +1,10 @@
-import { SyncHook } from 'tapable'
+import { SyncBailHook } from 'tapable'
 
-const hook = new SyncHook(['name', 'age'])
+const hook = new SyncBailHook(['name', 'age'])
 
 hook.tap('fn1', (name, age) => {
   console.log('fn1----->', name, age)
+  return !undefined
 })
 
 hook.tap('fn2', (name, age) => {
@@ -13,4 +14,5 @@ hook.tap('fn2', (name, age) => {
 hook.call('tyrion', 18)
 
 // fn1-----> tyrion 18
-// fn2-----> tyrion 18
+
+// 熔断钩子，返回非undefined，后边的钩子就不会执行
